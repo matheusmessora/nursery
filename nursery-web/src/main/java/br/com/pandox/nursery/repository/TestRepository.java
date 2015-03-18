9daaa7179cd9ca012373976a1c185a9b62763c89
Matheus Messora <matheus.messora.vpn@gmail.com>
2015-03-18 03:25:03 -0300

    #4 Testing Travis CI

--- nursery-web/src/main/java/br/com/pandox/nursery/repository/TestRepository.java
+++ nursery-web/src/main/java/br/com/pandox/nursery/repository/TestRepository.java
@@ -5,4 +5,5 @@ import br.com.pandox.nursery.entity.TestEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TestRepository extends CrudRepository<TestEntity, Long> {
+
 }
