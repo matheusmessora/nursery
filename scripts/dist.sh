916ee8ae8690bc5a4ae577d271c4dbbf024084e3
Matheus Messora <tqi_mmessora@uolinc.com>
2015-04-02 14:18:01 -0300

    #16 scripts for continuous delivery

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
-echo "Packing into JAR files"
+rm -rfv target/
+rm -rfv dist/
+
+echo "Packing JAR files"
 mvn clean compile assembly:single
 echo "Creating dist folder"
 mkdir dist
 cp target/nursery-web-jar-with-dependencies.jar dist -v
+
+echo "Copying scripts"
+cp scripts dist -vr
+
+echo "Copying scripts"
+cp scripts dist -vr
+
+echo "Zipping dist folder"
+tar -cf nursery.tar dist
